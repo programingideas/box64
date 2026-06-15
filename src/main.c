319d1fc04ac7a38f0830a85d9a0568972b727fa3
Johnraimi64 <106461618+Johnraimi64@users.noreply.github.com>
2026-06-15 16:05:40 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include "core.h"
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, const char **argv, char **env) {
+
+    // --- CUSTOM BLACK OPS 2 BYPASS PATCH START ---
+    if (argc > 1 && argv[1] != NULL && (strstr(argv[1], "t6sp.exe") || strstr(argv[1], "t6mp.exe") || strstr(argv[1], "t6zm.exe"))) {
+        setenv("BOX64_DYNAREC_FASTROUND", "1", 1);
+        setenv("BOX64_DYNAREC_WEAKBARRIER", "0", 1);
+    }
+    // --- CUSTOM BLACK OPS 2 BYPASS PATCH END ---
 
     x64emu_t* emu = NULL;
     elfheader_t* elf_header = NULL;
