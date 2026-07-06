e28e13bb8853bd2ac82f3cdafffba324c0037b47
Progamingideas <106461618+programingideas@users.noreply.github.com>
2026-07-06 19:23:12 +0100

    More Global Flags for all Black Ops 2 exe

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-// Stark Mode Global Flag for Black Ops 2
+// Declare the flag globally so the rest of Box64's source files can read it
 int box64_t6sp_workaround = 0;
 
 int main(int argc, const char **argv, char **env) {
@@ -12,13 +12,16 @@ int main(int argc, const char **argv, char **env) {
     // Scan arguments to see if a Black Ops 2 executable is trying to boot
     for (int i = 0; i < argc; ++i) {
         if (argv[i]) {
-            // Create a lowercase copy of the argument for robust matching
-            char *lower_arg = strdup(argv[i]);
+            // Use standard C-compliant length scanning to prevent strdup crashing
+            size_t len = strlen(argv[i]);
+            char *lower_arg = (char*)malloc(len + 1);
             if (lower_arg) {
-                for (int j = 0; lower_arg[j]; j++) {
-                    lower_arg[j] = tolower((unsigned char)lower_arg[j]);
+                for (size_t j = 0; j < len; j++) {
+                    lower_arg[j] = tolower((unsigned char)argv[i][j]);
                 }
+                lower_arg[len] = '\0';
 
+                // Check cleanly across all Black Ops 2 core executables
                 if (strstr(lower_arg, "t6sp.exe") || strstr(lower_arg, "t6zm.exe") || strstr(lower_arg, "t6mp.exe")) {
                     box64_t6sp_workaround = 1;
                     printf("[BOX64] Black Ops 2 workaround ACTIVATED via argv[%d]: %s\n", i, argv[i]);
